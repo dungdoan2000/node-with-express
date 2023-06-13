75de31086329f65bb3f4fede0ab647c05347c9c6
quocdung2000 <dung.doan@marahub.com>
2023-06-13 17:03:13 +0700

    new file search.hbs

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ app.get("/", (req,res)=>{
     res.render('home')
 });
 
+app.get("/search", (req,res)=>{
+    res.render('search')
+});
+
 app.get("/news", (req,res)=>{
     res.render('news')
 });
